85ef6568f7bf6b9a613b74d5c1454696f612a10c
pierre3 <pierre3@outlook.com>
2015-11-13 22:59:36 +0900

    initial commit

---
+++ EtwStream.PowerShell/GetTraceEventStream.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.Diagnostics.Tracing;
+using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Management.Automation;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace EtwStream.PowerShell
+{
+    [Cmdlet(VerbsCommon.Get, "TraceEventStream", DefaultParameterSetName = "nameOrGuid")]
+    public class GetTraceEvent : PSCmdlet
+    {
+        private CompositeDisposable disposable = new CompositeDisposable();
+
+        [ValidateSet(
+            nameof(WellKnownEventSources.AspNetEventSource),
+            nameof(WellKnownEventSources.ConcurrentCollectionsEventSource),
+            nameof(WellKnownEventSources.FrameworkEventSource),
+            nameof(WellKnownEventSources.PinnableBufferCacheEventSource),
+            nameof(WellKnownEventSources.PlinqEventSource),
+            nameof(WellKnownEventSources.SqlEventSource),
+            nameof(WellKnownEventSources.SynchronizationEventSource),
+            nameof(WellKnownEventSources.TplEventSource))]
+        [Parameter(Position = 0, ParameterSetName = "wellKnown", Mandatory = true)]
+        public string WellKnownEventSource { get; set; }
+
+        [Parameter(Position = 0, ParameterSetName = "nameOrGuid", Mandatory = true)]
+        public string NameOrGuid { get; set; }
+
+        [Parameter]
+        public SwitchParameter DumpWithColor { get; set; }
+
+        [Parameter]
+        public TraceEventLevel TraceLevel { get; set; } = TraceEventLevel.Verbose;
+
+
+        protected override void ProcessRecord()
+        {
+            IObservable<TraceEvent> listener = (ParameterSetName == "wellKnown")
+                ? GetWellKnownEventListener(WellKnownEventSource)
+                : ObservableEventListener.FromTraceEvent(NameOrGuid);
+            
+            var q = new BlockingCollection<Action>();
+            Exception exception = null;
+
+            var d = listener
+                .Where(x=> Process.GetCurrentProcess().Id != x.ProcessID)
+                .Where(x=>x.Level <= TraceLevel)
+                .Subscribe(
+                x =>
+                {
+                    q.Add(() =>
+                    {
+                        var item = new PSTraceEvent(x, Host.UI);
+                        if (DumpWithColor.IsPresent)
+                        {
+                            item.DumpWithColor();
+                        }
+                        else
+                        {
+                            WriteObject(item);
+                        }
+                        WriteVerbose(item.DumpPayloadOrMessage());
+                    });
+                },
+                e =>
+                {
+                    exception = e;
+                    q.CompleteAdding();
+                }, q.CompleteAdding);
+
+
+            disposable.Add(d);
+            var cts = new CancellationTokenSource();
+            disposable.Add(new CancellationDisposable(cts));
+            foreach (var act in q.GetConsumingEnumerable(cts.Token))
+            {
+                act();
+            }
+
+            if (exception != null)
+            {
+                ThrowTerminatingError(new ErrorRecord(exception, "1", ErrorCategory.OperationStopped, null));
+            }
+        }
+
+        protected override void StopProcessing()
+        {
+            disposable.Dispose();
+        }
+
+        private IObservable<TraceEvent> GetWellKnownEventListener(string wellKnownEventSource)
+        {
+            switch (wellKnownEventSource)
+            {
+                case nameof(WellKnownEventSources.AspNetEventSource):
+                    return ObservableEventListener.FromTraceEvent(WellKnownEventSources.AspNetEventSource);
+                case nameof(WellKnownEventSources.ConcurrentCollectionsEventSource):
+                    return ObservableEventListener.FromTraceEvent(WellKnownEventSources.ConcurrentCollectionsEventSource);
+                case nameof(WellKnownEventSources.FrameworkEventSource):
+                    return ObservableEventListener.FromTraceEvent(WellKnownEventSources.FrameworkEventSource);
+                case nameof(WellKnownEventSources.PinnableBufferCacheEventSource):
+                    return ObservableEventListener.FromTraceEvent(WellKnownEventSources.PinnableBufferCacheEventSource);
+                case nameof(WellKnownEventSources.PlinqEventSource):
+                    return ObservableEventListener.FromTraceEvent(WellKnownEventSources.PlinqEventSource);
+                case nameof(WellKnownEventSources.SqlEventSource):
+                    return ObservableEventListener.FromTraceEvent(WellKnownEventSources.SqlEventSource);
+                case nameof(WellKnownEventSources.SynchronizationEventSource):
+                    return ObservableEventListener.FromTraceEvent(WellKnownEventSources.SynchronizationEventSource);
+                case nameof(WellKnownEventSources.TplEventSource):
+                    return ObservableEventListener.FromTraceEvent(WellKnownEventSources.TplEventSource);
+                default:
+                    return Observable.Empty<TraceEvent>();
+            }
+        }
+    }
+    
+}
---
+++ EtwStream.PowerShell/PSTraceEvent.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Diagnostics.Tracing;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management.Automation.Host;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EtwStream.PowerShell
+{
+    public class PSTraceEvent
+    {
+        public DateTime TimeStamp { get; }
+        public TraceEventLevel Level { get; }
+        public string ProcessName { get; }
+        public string ProviderName { get; }
+        public string EventName { get; }
+        public string PayloadOrMessage { get; }
+
+        public TraceEvent Source { get; }
+
+        private PSHostUserInterface hostUI;
+
+        public PSTraceEvent(TraceEvent traceEvent, PSHostUserInterface hostUI)
+        {
+            this.hostUI = hostUI;
+
+            Source = traceEvent;
+
+            TimeStamp = Source.TimeStamp;
+            ProcessName = Process.GetProcessById(Source.ProcessID)?.ProcessName ?? "";
+            ProviderName = Source.ProviderName;
+            EventName = Source.EventName;
+            Level = Source.Level;
+            PayloadOrMessage = Source.DumpPayloadOrMessage();
+        }
+
+        public string Dump(bool includePrettyPrint = false, bool truncateDump = false)
+        {
+            return Source.Dump(includePrettyPrint, truncateDump);
+        }
+
+        public void DumpPayload()
+        {
+            Source.DumpPayload();
+        }
+
+        public string DumpPayloadOrMessage()
+        {
+            return Source.DumpPayloadOrMessage();
+        }
+
+        public ConsoleColor? GetColorMap(bool isBackgroundWhite)
+        {
+            return Source.GetColorMap(isBackgroundWhite);
+        }
+
+        public void DumpWithColor(bool withProviderName = false, bool withProcesName = false)
+        {
+
+            var proces = (withProcesName) ? $"[{ProcessName}]" : "";
+            var provider = (withProcesName) ? $"[{ProviderName}]" : "";
+            var message = $"[{Source.TimeStamp}]{proces}{provider}[{Source.EventName}]:{PayloadOrMessage}";
+            var foreColor = GetColorMap(hostUI.RawUI.BackgroundColor == ConsoleColor.White) ?? hostUI.RawUI.ForegroundColor;
+
+            hostUI.WriteLine(foreColor, hostUI.RawUI.BackgroundColor, message);
+        }
+    }
+}
