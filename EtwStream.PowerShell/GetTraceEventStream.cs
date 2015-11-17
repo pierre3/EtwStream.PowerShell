4f426051f1f92b78c9b507e5d2f050162a22ad9e
pierre3 <pierre3@outlook.com>
2015-11-17 22:29:38 +0900

    add IISEventSources parameter to GetTraceEvent

--- EtwStream.PowerShell/GetTraceEventStream.cs
+++ EtwStream.PowerShell/GetTraceEventStream.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Diagnostics.Tracing;
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Management.Automation;
@@ -16,6 +15,9 @@ namespace EtwStream.PowerShell
     {
         private CompositeDisposable disposable = new CompositeDisposable();
 
+        [Parameter(Position = 0, ParameterSetName = "nameOrGuid", Mandatory = true, ValueFromPipeline = true)]
+        public string[] NameOrGuid { get; set; }
+
         [ValidateSet(
             nameof(WellKnownEventSources.AspNetEventSource),
             nameof(WellKnownEventSources.ConcurrentCollectionsEventSource),
@@ -28,8 +30,20 @@ namespace EtwStream.PowerShell
         [Parameter(Position = 0, ParameterSetName = "wellKnown", Mandatory = true, ValueFromPipeline = true)]
         public string[] WellKnownEventSource { get; set; }
 
-        [Parameter(Position = 0, ParameterSetName = "nameOrGuid", Mandatory = true, ValueFromPipeline = true)]
-        public string[] NameOrGuid { get; set; }
+
+        [ValidateSet(
+            nameof(IISEventSources.AspDotNetEvents),
+            nameof(IISEventSources.HttpEvent),
+            nameof(IISEventSources.HttpLog),
+            nameof(IISEventSources.HttpService),
+            nameof(IISEventSources.IISAppHostSvc),
+            nameof(IISEventSources.IISLogging),
+            nameof(IISEventSources.IISW3Svc),
+            nameof(IISEventSources.RuntimeWebApi),
+            nameof(IISEventSources.RuntimeWebHttp))]
+        [Parameter(Position = 0, ParameterSetName = "IIS", Mandatory = true, ValueFromPipeline = true)]
+        public string[] IISEventSource { get; set; }
+
 
         [Parameter]
         public SwitchParameter DumpWithColor { get; set; }
@@ -37,29 +51,30 @@ namespace EtwStream.PowerShell
         [Parameter]
         public TraceEventLevel TraceLevel { get; set; } = TraceEventLevel.Verbose;
 
-        private List<IObservable<TraceEvent>> listeners = new List<IObservable<TraceEvent>>();
-
+        private IObservable<TraceEvent> listener = Observable.Empty<TraceEvent>();
 
         protected override void ProcessRecord()
         {
-            if (ParameterSetName == "wellKnown")
+            switch (ParameterSetName)
             {
-
-                listeners.AddRange(WellKnownEventSource.Select(x => GetWellKnownEventListener(x)).Where(x => x != null));
-            }
-            else
-            {
-                listeners.AddRange(NameOrGuid.Select(x => ObservableEventListener.FromTraceEvent(x)));
+                case "wellKnown":
+                    listener = listener.Merge(WellKnownEventSource.Select(x => GetWellKnownEventListener(x)).Merge());
+                    break;
+                case "IIS":
+                    listener = listener.Merge(IISEventSource.Select(x => GetIISEventListener(x)).Merge());
+                    break;
+                default:
+                    listener = listener.Merge(NameOrGuid.Select(x => ObservableEventListener.FromTraceEvent(x)).Merge());
+                    break;
             }
         }
 
         protected override void EndProcessing()
         {
-
             var q = new BlockingCollection<Action>();
             Exception exception = null;
 
-            var d = listeners.Merge()
+            var d = listener
                 .Where(x => Process.GetCurrentProcess().Id != x.ProcessID)
                 .Where(x => x.Level <= TraceLevel)
                 .Subscribe(
@@ -126,7 +141,36 @@ namespace EtwStream.PowerShell
                 case nameof(WellKnownEventSources.TplEventSource):
                     return ObservableEventListener.FromTraceEvent(WellKnownEventSources.TplEventSource);
                 default:
-                    return null;
+                    return Observable.Empty<TraceEvent>();
+            }
+        }
+
+        private IObservable<TraceEvent> GetIISEventListener(string iisEventSource)
+        {
+
+            switch (iisEventSource)
+            {
+                case nameof(IISEventSources.AspDotNetEvents):
+                    return ObservableEventListener.FromTraceEvent(IISEventSources.AspDotNetEvents);
+                case nameof(IISEventSources.HttpEvent):
+                    return ObservableEventListener.FromTraceEvent(IISEventSources.HttpEvent);
+                case nameof(IISEventSources.HttpLog):
+                    return ObservableEventListener.FromTraceEvent(IISEventSources.HttpLog);
+                case nameof(IISEventSources.HttpService):
+                    return ObservableEventListener.FromTraceEvent(IISEventSources.HttpService);
+                case nameof(IISEventSources.IISAppHostSvc):
+                    return ObservableEventListener.FromTraceEvent(IISEventSources.IISAppHostSvc);
+                case nameof(IISEventSources.IISLogging):
+                    return ObservableEventListener.FromTraceEvent(IISEventSources.IISLogging);
+                case nameof(IISEventSources.IISW3Svc):
+                    return ObservableEventListener.FromTraceEvent(IISEventSources.IISW3Svc);
+                case nameof(IISEventSources.RuntimeWebApi):
+                    return ObservableEventListener.FromTraceEvent(IISEventSources.RuntimeWebApi);
+                case nameof(IISEventSources.RuntimeWebHttp):
+                    return ObservableEventListener.FromTraceEvent(IISEventSources.RuntimeWebHttp);
+                default:
+                    return Observable.Empty<TraceEvent>();
+
             }
         }
     }
